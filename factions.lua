7d1652f2b601778227c65ff2077de4da7dfbd957
Luexks <ALX5555@icloud.com>
2024-09-03 07:40:28 +0100

    tweaks

--- factions.lua
+++ factions.lua
@@ -4,12 +4,12 @@
 		playable=2
 		primaries=3
 		start="99_1"
-		color0=0xff99ee
-		color1=0xb6ccdb
-		color2=0x0a0529
 		explosionColor0=0x797cfd
         explosionColor1=0xff7efd
 		thrustSFX=1
 		aiflags=FLOCKING|RECKLESS|CAUTIOUS|SOCIAL|WANDER|HATES_PLANTS|FORGIVING|TRACTOR_TRANSIENT|DODGES|SMART_FIRE
+		color1=0xDB513D
+		color2=0xDBA83D
+		color3=0x0a0529
 	}
 }
